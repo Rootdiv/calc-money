bcd9873f8d792145e0d20c3534025b9aed96793f
Rootdiv <rootdiv@mail.ru>
2021-09-02 15:31:45 +0300

    feat: web application structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './normalize.css';
+import './style.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// const FunctionComponents = props => React.createElement('h1', { id: 'hi' }, props.text);
+// const Header = ({ text }) => <h1 id="hi">{text}</h1>;
+
+// const Element = React.createElement('p', null, 'Параграф');
+// const Element = <p>Параграф</p>;
+
+// class Main extends React.Component {
+//   render() {
+//     return (
+//       <div className="main">
+//         <Header text={'Свойство текст ' + this.props.count} />
+//         {Element}
+//       </div>
+//     );
+//     // return React.createElement('div', { className: 'main' },
+//     //   React.createElement(FunctionComponents, { text: 'Свойство текст' }), Element);
+//   }
+// }
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <Main count={5} /> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
